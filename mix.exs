363345c98fa7e9bc36df356b6e75db9cf0712155
Fadhil Luqman <fadhil.luqman@gmail.com>
2018-07-19 18:16:22 +0800

    Bump version to 0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WebsFlyer.Mixfile do
   def project do
     [
       app: :webs_flyer,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
